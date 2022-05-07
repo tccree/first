9ac4c1e68fb539512aeb3949ff5422bedb91e659
qmx <1914815536@qq.com>
2022-05-07 17:31:17 +0800

    升级，增加本地文件工具方法

--- src/main/java/mollie/basic/common/utils/FileUtil.java
+++ src/main/java/mollie/basic/common/utils/FileUtil.java
@@ -3,6 +3,7 @@ package mollie.basic.common.utils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.UUID;
 
 /**
  * 文件工具类
@@ -21,6 +22,28 @@ public class FileUtil {
             dest.getParentFile().mkdirs();
         }
         file.transferTo(dest);
+    }
+
+    public static final String SAVE_PATH = "D:/mollie-file";
+
+    public static String uploadImg(MultipartFile multipartFile)throws Exception{
+        String filename = "";
+        //生成uuid作为文件名
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        //获取文件类型
+        String contentType = multipartFile.getContentType();
+        //获取文件后缀名
+        String suffixName = contentType.substring(contentType.indexOf("/")+1);
+        //生成新的文件名
+        filename = uuid+"."+suffixName;
+
+        File dest = new File(SAVE_PATH,filename);
+        //保存文件到路径
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        multipartFile.transferTo(dest);
+        return filename;
     }
 
 }
